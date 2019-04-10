1f850dff6ae337928508ef983d1ac403893b7a7e
keefe <keefe@whale.im>
2019-04-10 14:44:05 +0800

    add sort

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
 #coding:utf-8
 import json
+import os
 def generateOutputData(keyName):
 	return "{{ $t('%s') }}"%keyName
-inputPath = 'data/test.txt'
-outputPath = 'data/out.txt'
-jsonPath = 'data/cn.json'
-inputFile = open(inputPath,encoding = 'utf-8')
-outputFile = open(outputPath, 'w', encoding = 'utf-8')
+
+def loadFiles(path):
+	files = os.listdir(path)
+	return files
+
+def createFolder(folder_name):
+	if not os.path.exists(folder_name):
+		os.mkdir(folder_name)
+
+def get_key (dict, value):
+	return [k for k, v in dict.items() if v == value]
+
+def getSortedMapKey(keyMap):
+	valueList = []
+	for keyName in keyMap:
+		valueList.append(keyMap[keyName])
+	sortedValueList = valueList.sort(key=lambda x:len(x))
+	sortedKeyList = []
+	for value in valueList:
+		sortedKeyList.append(get_key(keyMap, value))
+	return sortedKeyList
+
+inputFolder = 'data/'
+outputFolder = 'output/'
+createFolder(outputFolder)
+jsonPath = 'json/cn.json'
 jsonFile = open(jsonPath)
 jsonData = jsonFile.read()
 keyMap = json.loads(jsonData)
-data = inputFile.read()
-for keyName in keyMap:
-	candidateData = keyMap[keyName]
-	print('candidate: %s key: %s'%(candidateData, keyName))
-		
-	# find the idx
-	idx = data.find(candidateData)
-	print('idx: %d'%idx)
-	if idx == -1:
-		continue
-	print('data: %s' %data[idx:idx + len(candidateData)])
+sortedKeyList = getSortedMapKey(keyMap)
 
-	# substitute
-	data = data[:idx]+ generateOutputData(keyName) + data[idx + len(candidateData):]
-print('output:%s'%data)
-outputFile.write(data)
-inputFile.close()
-outputFile.close()
+inputFiles = loadFiles(inputFolder)
+for FileName in inputFiles:
+	inputPath = os.path.join(inputFolder, FileName)
+	inputFile = open(inputPath,encoding = 'utf-8')
+
+	outputPath = os.path.join(outputFolder, FileName)
+	outputFile = open(outputPath, 'w', encoding = 'utf-8')
+
+	data = inputFile.read()
+
+	for keyNameList in sortedKeyList:
+		keyName = keyNameList[0]
+		candidateData = keyMap[keyName]
+		print('candidate: %s key: %s'%(candidateData, keyName))
+			
+		# find the idx
+		idx = data.find(candidateData)
+		print('idx: %d'%idx)
+		if idx == -1:
+			continue
+		print('data: %s' %data[idx:idx + len(candidateData)])
+
+		# substitute
+		data = data[:idx]+ generateOutputData(keyName) + data[idx + len(candidateData):]
+	print('output:%s'%data)
+	outputFile.write(data)
+	inputFile.close()
+	outputFile.close()
