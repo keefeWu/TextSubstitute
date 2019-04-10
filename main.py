a800b4e023d9670ce5db5f22915624603e68bc75
keefe <keefe@whale.im>
2019-04-10 14:11:44 +0800

    load from json

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 #coding:utf-8
+import json
 def generateOutputData(keyName):
 	return "{{ $t('%s') }}"%keyName
 inputPath = 'data/test.txt'
-file = open(inputPath,encoding = 'utf-8')
-data = file.read()
-# print(data)
-candidateData = '账号或密码错误，请重新输入'
-keyName = 'account_password_error_please_input_again'
-mould = "{{ $t('the_account_cannot_be_null') }}"
-# find the idx
-idx = data.find(candidateData)
-print('idx: %d'%idx)
-print('data: %s' %data[idx:idx + len(candidateData)])
-# substitute
-outputData = data[:idx]+ generateOutputData(keyName) + data[idx + len(candidateData):]
-print('output:%s'%outputData)
-file.close()
+outputPath = 'data/out.txt'
+jsonPath = 'data/cn.json'
+inputFile = open(inputPath,encoding = 'utf-8')
+outputFile = open(outputPath, 'w', encoding = 'utf-8')
+jsonFile = open(jsonPath)
+jsonData = jsonFile.read()
+keyMap = json.loads(jsonData)
+data = inputFile.read()
+for keyName in keyMap:
+	candidateData = keyMap[keyName]
+	print('candidate: %s key: %s'%(candidateData, keyName))
+		
+	# find the idx
+	idx = data.find(candidateData)
+	print('idx: %d'%idx)
+	if idx == -1:
+		continue
+	print('data: %s' %data[idx:idx + len(candidateData)])
+
+	# substitute
+	data = data[:idx]+ generateOutputData(keyName) + data[idx + len(candidateData):]
+print('output:%s'%data)
+outputFile.write(data)
+inputFile.close()
+outputFile.close()
