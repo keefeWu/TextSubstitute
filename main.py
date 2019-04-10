1e24766db8feafac732ab097567125ab0c49e985
keefe <keefe@whale.im>
2019-04-10 11:55:44 +0800

    find single text

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-
+#coding:utf-8
 inputPath = 'data/test.txt'
 file = open(inputPath)
 data = file.read()
 print(data)
+candidateData = '账号或密码错误，请重新输入'
+idx = data.find(candidateData)
+print('idx: %d'%idx)
+print('data: %s' %data[idx:idx + len(candidateData)])
 file.close()
